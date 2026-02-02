35a55e872d24f9bdce3e9ab0d2ab7bbcf0dc34ab
Manuel Spigolon <manuel.spigolon@nearform.com>
2026-02-02 20:17:31 +0100

    chore: fuzzing manual test (#59)

---
+++ benchmarks/fuzzer-check-errors.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const { FuzzedDataProvider } = require('@jazzer.js/core')
+const fastContentTypeParse = require('../index.js')
+
+function fuzz (data) {
+  const provider = new FuzzedDataProvider(data)
+
+  const fmtString = provider.consumeRemainingAsString()
+  try {
+    fastContentTypeParse.parse(fmtString)
+  } catch (error) {
+    // Ignore all expected errors
+    if (!(error instanceof TypeError)) {
+      throw error
+    }
+  }
+}
+
+module.exports = { fuzz }
---
+++ benchmarks/fuzzer-check-performance.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const { FuzzedDataProvider } = require('@jazzer.js/core')
+const fastContentTypeParse = require('../index.js')
+
+function fuzz (data) {
+  const provider = new FuzzedDataProvider(data)
+
+  const fmtString = provider.consumeRemainingAsString()
+  // Ignore errors; we're interested in performance slowdown
+  fastContentTypeParse.safeParse(fmtString)
+}
+
+module.exports = { fuzz }
